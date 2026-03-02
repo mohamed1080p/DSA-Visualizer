558f99dc7d7c84af24630ac6c0ffa5bfc043ce25
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-02 06:39:10 +0200

    Creating Generic Repository

---
+++ Shared/DTOs/IdentityDTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shared.DTOs.IdentityDTOs
+{
+    public class UserDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}

2f4b150c982800dffb62d35b8660b0a39e7f4997
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-04 09:13:56 +0200

    Implemented AuthService and generated JWT

--- Shared/DTOs/IdentityDTOs/RegisterDTO.cs
+++ Shared/DTOs/IdentityDTOs/RegisterDTO.cs
@@ -9,5 +9,6 @@ namespace Shared.DTOs.IdentityDTOs
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
     }
 }

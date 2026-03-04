2b357298f1468f237cdf1c158d30ba9e93c6b4ff
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-04 07:06:09 +0200

    Created RefreshToken Repository

---
+++ Core/Domain/Contracts/IRefreshTokenRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using Domain.Models.IdentityModule;
+
+namespace Domain.Contracts
+{
+    public interface IRefreshTokenRepository
+    {
+        // get refresh token
+        Task<RefreshToken?> GetRefreshTokenAsync(string token);
+
+        // add refresh token 
+        Task AddRefreshTokenAsync(RefreshToken refreshToken);
+
+        // revoke refresh token
+        Task RevokeRefreshTokenForUser(string id);
+    }
+}
--- Core/Domain/Models/IdentityModule/RefreshToken.cs
+++ Core/Domain/Models/IdentityModule/RefreshToken.cs
@@ -4,7 +4,8 @@ namespace Domain.Models.IdentityModule
     public class RefreshToken
     {
         public int Id { get; set; }
-        public string Token { get; set; } = string.Empty;     
+        public string Token { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime ExpiresAt { get; set; }
         public bool IsRevoked { get; set; } = false;
 

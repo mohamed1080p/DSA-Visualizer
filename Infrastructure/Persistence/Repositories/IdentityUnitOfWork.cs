ab7dbd88e69489fbb341aa820b3c6e51fcb75cac
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-06 08:48:39 +0200

    Adding UnitofWork, and IdentityUnitOfWork

---
+++ Infrastructure/Persistence/Repositories/IdentityUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿
+using Domain.Contracts;
+using Persistence.Identity;
+
+namespace Persistence.Repositories
+{
+    public class IdentityUnitOfWork(ApplicationIdentityDbContext _identityDbContext) : IIdentityUnitOfWork
+    {
+        private readonly Lazy<IRefreshTokenRepository> _refreshTokenRepository = new(() => new RefreshTokenRepository(_identityDbContext));
+        public IRefreshTokenRepository RefreshTokenRepository => _refreshTokenRepository.Value;
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _identityDbContext.SaveChangesAsync();
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await _identityDbContext.DisposeAsync();
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿
+using Domain.Contracts;
+using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(ApplicationDbContext _applicationDbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = new();
+
+       
+
+        public IGenericRepository<TEntity, TKey> genericRepository<TEntity, TKey>() where TEntity : class
+        {
+            string name = typeof(TEntity).Name;
+            if(!_repositories.ContainsKey(name))
+            {
+                _repositories[name] = new GenericRepository<TEntity, TKey>(_applicationDbContext);
+            }
+            return (IGenericRepository<TEntity, TKey>)_repositories[name];
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _applicationDbContext.SaveChangesAsync();
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await _applicationDbContext.DisposeAsync();
+        }
+
+        
+    }
+}

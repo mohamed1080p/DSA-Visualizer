f328814e8487d8b2a2967aac93e25e2072cd72ad
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-12 23:57:19 +0200

    Added Topic Service

--- Core/Services/ServiceManager.cs
+++ Core/Services/ServiceManager.cs
@@ -11,5 +11,8 @@ namespace Services
     {
         private readonly Lazy<IAuthService> _authService = new(() => new AuthService(_userManager, _unitOfWork, _configuration));
         public IAuthService AuthService => _authService.Value;
+
+        private readonly Lazy<ITopicService> _topicService = new(() => new TopicService(_unitOfWork));
+        public ITopicService TopicService => _topicService.Value;
     }
 }

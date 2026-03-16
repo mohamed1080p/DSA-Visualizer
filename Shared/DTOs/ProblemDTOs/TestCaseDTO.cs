25663ac592d25efd5c1fd6e8be8a8511c334909e
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-17 01:02:54 +0200

    Created ProblemDTOs

---
+++ Shared/DTOs/ProblemDTOs/TestCaseDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shared.DTOs.ProblemDTOs
+{
+    public class TestCaseDTO
+    {
+        public int Id { get; set; }
+        public string Input { get; set; } = string.Empty;
+        public string ExpectedOutput { get; set; } = string.Empty;
+    }
+}

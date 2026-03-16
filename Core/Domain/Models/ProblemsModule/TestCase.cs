c1c65a9bf82579c8244d1fe59b0e2a9b7306b293
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-16 23:41:52 +0200

    Created Problems Module models and its configurations

---
+++ Core/Domain/Models/ProblemsModule/TestCase.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Domain.Models.ProblemsModule
+{
+    public class TestCase
+    {
+        public int Id { get; set; }
+        public string Input { get; set; } = string.Empty;
+        public string ExpectedOutput { get; set; } = string.Empty;
+
+        // Hidden test cases are used server-side only during code execution.
+        // They are never sent to the frontend.
+        public bool IsHidden { get; set; } = false;
+
+        /////////////////////////////////////////////////////////////
+        public int ProblemId { get; set; }
+        public Problem Problem { get; set; } = default!;
+    }
+}

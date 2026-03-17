b7b698fea9622018b71cfba0a5b1d86b413ba37d
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-17 23:54:27 +0200

    Fixing some bugs and seeding some problems for testing

--- Infrastructure/Presentation/Controllers/ProblemsController.cs
+++ Infrastructure/Presentation/Controllers/ProblemsController.cs
@@ -8,16 +8,8 @@ namespace Presentation.Controllers
     public class ProblemsController(IServiceManager _serviceManager):ControllerBase
     {
         // GET api/problems
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProblemDTO>>> GetAll()
-        {
-            var problems = await _serviceManager.ProblemService.GetAllAsync();
-            return Ok(problems);
-        }
-
-        // GET api/problems/filter?searchTerm=two+sum&categoryId=1
-        [HttpGet("filter")]
-        public async Task<ActionResult<IEnumerable<ProblemDTO>>> Filter([FromQuery] ProblemQueryParametersDTO parameters)
+        [HttpGet("problems")]
+        public async Task<ActionResult<IEnumerable<ProblemDTO>>> GetAll([FromQuery] ProblemQueryParametersDTO parameters)
         {
             var problems = await _serviceManager.ProblemService.GetAllAsync(parameters);
             return Ok(problems);

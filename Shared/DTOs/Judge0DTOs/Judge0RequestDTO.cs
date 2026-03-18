fe8d1bc152194de576ccd3dbef37f10ea958f4b5
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-03-18 05:20:03 +0200

    Adding Judge0 Service to application for code execution

---
+++ Shared/DTOs/Judge0DTOs/Judge0RequestDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Shared.DTOs.Judge0DTOs
+{
+    public class Judge0RequestDTO
+    {
+        public string SourceCode { get; set; } = string.Empty;
+        public int LanguageId { get; set; }
+        public string Stdin { get; set; } = string.Empty;
+        public double CpuTimeLimit { get; set; }
+        public int MemoryLimit { get; set; }
+    }
+}

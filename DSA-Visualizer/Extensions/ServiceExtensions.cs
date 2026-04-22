09a6bb9501d37d524d51fa12752e87092f857e30
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-04-23 01:52:07 +0200

    refactor: extract service registrations from program.cs into extension methods

---
+++ DSA-Visualizer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,108 @@
+﻿using Domain.Contracts;
+using Domain.Models.IdentityModule;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Persistence.Data;
+using Persistence.Data.Seeds;
+using Persistence.Repositories;
+using Services;
+using ServicesAbstraction;
+using System.Text;
+
+namespace DSA_Visualizer.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddSwaggerServices(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    public static void AddIdentityServices(this IServiceCollection services)
+    {
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequiredLength = 8;
+
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+            options.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+    }
+
+    public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!)),
+                ClockSkew = TimeSpan.Zero
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnChallenge = async context =>
+                {
+                    context.HandleResponse();
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        StatusCode = StatusCodes.Status401Unauthorized,
+                        Message = "You are not authorized. Please login first."
+                    });
+                }
+            };
+        })
+        .AddGoogle(googleOptions =>
+        {
+            googleOptions.ClientId = configuration["ExternalAuth:Google:ClientId"]!;
+            googleOptions.ClientSecret = configuration["ExternalAuth:Google:ClientSecret"]!;
+        })
+        .AddGitHub(githubOptions =>
+        {
+            githubOptions.ClientId = configuration["ExternalAuth:GitHub:ClientId"]!;
+            githubOptions.ClientSecret = configuration["ExternalAuth:GitHub:ClientSecret"]!;
+            githubOptions.Scope.Add("user:email");
+        });
+    }
+
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IServiceManager, ServiceManager>();
+        services.AddScoped<DataSeeding>();
+        services.AddScoped<ICodeExecutionService, CodeExecutionService>();
+    }
+}
--- DSA-Visualizer/Program.cs
+++ DSA-Visualizer/Program.cs
@@ -1,16 +1,6 @@
-using Domain.Contracts;
 using Domain.Exceptions;
-using Domain.Models.IdentityModule;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Persistence.Data;
+using DSA_Visualizer.Extensions;
 using Persistence.Data.Seeds;
-using Persistence.Repositories;
-using Services;
-using ServicesAbstraction;
-using System.Text;
 
 namespace DSA_Visualizer
 {
@@ -20,98 +10,15 @@ namespace DSA_Visualizer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // ------------------- Controllers and Swagger -------------------
             builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerServices();
             builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             builder.Services.AddProblemDetails();
+            builder.Services.AddDatabaseServices(builder.Configuration);
+            builder.Services.AddIdentityServices();
+            builder.Services.AddJwtAuthentication(builder.Configuration);
+            builder.Services.AddApplicationServices();
 
-            // ------------------- DB Contexts -------------------
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            // ------------------- Identity -------------------
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                // Password rules
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequiredLength = 8;
-
-                // Lockout settings
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
-
-                // User settings
-                options.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
-
-            // ------------------- JWT Authentication Configurations -------------------
-            var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings["Issuer"],
-                    ValidAudience = jwtSettings["Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!)),
-                    ClockSkew = TimeSpan.Zero
-                };
-                options.Events = new JwtBearerEvents
-                {
-                    OnChallenge = async context =>
-                    {
-                        context.HandleResponse();
-                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        context.Response.ContentType = "application/json";
-
-                        await context.Response.WriteAsJsonAsync(new
-                        {
-                            StatusCode = StatusCodes.Status401Unauthorized,
-                            Message = "You are not authorized. Please login first."
-                        });
-                    }
-                };
-            })
-            .AddGoogle(googleOptions =>
-            {
-                googleOptions.ClientId = builder.Configuration["ExternalAuth:Google:ClientId"]!;
-                googleOptions.ClientSecret = builder.Configuration["ExternalAuth:Google:ClientSecret"]!;
-            })
-            .AddGitHub(githubOptions =>
-            {
-                githubOptions.ClientId = builder.Configuration["ExternalAuth:GitHub:ClientId"]!;
-                githubOptions.ClientSecret = builder.Configuration["ExternalAuth:GitHub:ClientSecret"]!;
-                githubOptions.Scope.Add("user:email");
-            });
-
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<IServiceManager, ServiceManager>();
-            builder.Services.AddScoped<DataSeeding>();
-            builder.Services.AddScoped<ICodeExecutionService, CodeExecutionService>();
-
-
-
-
-
-            // ------------------- Application Pipeline -------------------
             var app = builder.Build();
 
             using (var scope = app.Services.CreateScope())
